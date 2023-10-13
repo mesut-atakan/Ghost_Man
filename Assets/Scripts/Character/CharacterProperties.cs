a6c5d1917cdfb55cb315287683ed58a2d9c9751e
mesut_atakan <mesut_atakan5@hotmail.com>
2023-10-13 15:24:47 +0300

    File Design

---
+++ Assets/Scripts/Character/CharacterProperties.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+
+
+namespace Character
+{
+    internal abstract class CharacterProperties : MonoBehaviour
+    {
+#region ||~~~~~~~~~~|| X ||~~~~~~~~~~|| SERIALIZE FIELDS ||~~~~~~~~~~|| X ||~~~~~~~~~~||
+
+        [Header("")]
+
+#endregion ||~~~~~~~~~~|| X ||~~~~~~~~~~|| XXXX ||~~~~~~~~~~|| X ||~~~~~~~~~~||
+    }
+}
